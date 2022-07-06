a1727ea1cbe4edabe32e2c3a9d35c785b30034f0
aaajr97 <aaajr97@outlook.com>
2022-07-06 17:24:21 +0100

    Added function that forms url and integrated it within the function that adds api data to an empty list

--- starwars_mongodb_task.py
+++ starwars_mongodb_task.py
@@ -20,17 +20,25 @@ def get_api_status_code(url: str):
     return api_status_code
 
 
+# Function that forms an api url using the HTTP url components
+def forming_url(i: int):
+    base_url = "https://swapi.dev/api"
+    endpoint = "/starships/?page="
+    page_number = i
+    api_url = f"{base_url}{endpoint}{page_number}"
+    return api_url
+
+
 # function that appends api data to list given that the response sends a 200 code
 # code iterates through different urls as the urls differ in page number
-def add_data_to_list():
-    url = "https://swapi.dev/api/starships/?page=1"
-    i = 1
+def add_data_to_list(i=1):
+    url = forming_url(i)
     while get_api_status_code(url) == 200:
         for ship_details in requests.get(url).json()["results"]:
             starship_list.append(ship_details)
         i += 1
-        url = f'https://swapi.dev/api/starships/?page={i}'
+        url = forming_url(i)
     return starship_list
 
 
-pprint(add_data_to_list())
+add_data_to_list()
